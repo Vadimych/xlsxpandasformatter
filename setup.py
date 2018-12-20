fc559b19efede3dd9e5e1a016b60cbc69b9f8685
Vadimych <login.vadim@gmail.com>
2018-12-20 23:25:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='xlsxpandasformatter',
+    version='0.0.1',
+    description='Excel Pandas Formatter',
+    license='MIT',
+    packages=['xlsxpandasformatter'],
+    url='https://github.com/Vadimych/xlsxpandasformatter.git'
+)
